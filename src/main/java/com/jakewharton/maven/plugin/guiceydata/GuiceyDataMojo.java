9afd2a399c9fa4e5da1e36add469fb75623ce0c7
Jake Wharton <jakewharton@gmail.com>
2010-10-21 17:51:59 -0400

    Beef up. Extensions, default files, and error checking.

--- src/main/java/com/jakewharton/maven/plugin/guiceydata/GuiceyDataMojo.java
+++ src/main/java/com/jakewharton/maven/plugin/guiceydata/GuiceyDataMojo.java
@@ -22,59 +22,127 @@ import com.lowereast.guiceymongo.data.generator.GuiceyDataGenerator;
 import java.io.File;
 
 /**
- * Goal which automatically regenerates 
- *
+ * Goal which automatically generates MongoDB DAO Java source files using
+ * GuiceyData definition files.
+ * 
  * @goal generate
  * @phase generate-sources
  * @author Jake Wharton
  */
 public class GuiceyDataMojo extends AbstractMojo {
-	
-    /**
-     * Location of the file.
-     * 
-     * @parameter expression="${project.build.sourceDirectory}"
-     * @required
-     */
-    private File outputDirectory;
-    
-    /**
-     * Output package
-     * 
-     * @parameter
-     * @package
-     */
-    private String outputPackage;
-    
-    /**
-     * <p>By convention field names are specified as field_name, which will
-	 * generate an object as <code>{'field_name': true}</code>.  Using this
-	 * option, <code>{'fieldName': true}</code> will be generated.</p>
+	/**
+	 * Default file extension to look for in directories.
+	 */
+	private static final String DEFAULT_EXTENSION = ".data";
+
+	/**
+	 * Message when no paths are explicitly defined in the pom.xml.
+	 */
+	private static final String MESSAGE_NO_PATHS = "No paths explicitly listed. Using source directory.";
+
+	/**
+	 * Message when no extensions are explicitly defined in the pom.xml.
+	 */
+	private static final String MESSAGE_NO_EXTENSIONS = "No extensions listed. Using default '" + GuiceyDataMojo.DEFAULT_EXTENSION + "'.";
+
+	/**
+	 * Message that generation is about to occur.
+	 */
+	private static final String MESSAGE_GENERATING = "Generating classes";
+
+	/**
+	 * Message when a fatal generation error has occurred.
+	 */
+	private static final String MESSAGE_ERROR = "Fatal error occured while generating classes.";
+
+
+
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${project.build.sourceDirectory}"
+	 * @required
+	 */
+	private File outputDirectory;
+
+	/**
+	 * Output package.
+	 * 
+	 * @parameter
+	 * @package
+	 */
+	private String outputPackage;
+
+	/**
+	 * By convention field names are specified as "field_name", which will
+	 * generate an object as <code>{'field_name': true}</code>. Using this
+	 * option, <code>{'fieldName': true}</code> will be generated.
 	 * 
 	 * @parameter default-value="false"
-     */
-    private boolean useCamelCase;
-    
-    /**
-     * List of .data files to parse.
-     * 
-     * @parameter
-     * @required
-     */
-    private String[] files;
+	 */
+	private boolean useCamelCase;
 
-    
-    
-    /**
-     * Run the goal.
-     */
-    public void execute() throws MojoExecutionException
-    {
-    	final GuiceyDataGenerator generator = new GuiceyDataGenerator();
-    	
-    	generator.setSourceDirectory(outputDirectory.getAbsolutePath());
-    	generator.setOutputPackage(this.outputPackage);
-    	
-    	generator.generate(this.useCamelCase, this.files);
-    }
+	/**
+	 * List of file or directory paths to parse.
+	 * 
+	 * @parameter
+	 */
+	private String[] paths;
+
+	/**
+	 * Path to the source directory. This is used as a default for the
+	 * <code>paths</code> parameter if no values were specified.
+	 * 
+	 * @parameter expression="${project.build.sourceDirectory}"
+	 * @required
+	 * @readonly
+	 */
+	private File defaultFileDirectory;
+
+	/**
+	 * List of extensions to parse when listing directories in the
+	 * <code>paths</code> parameter.
+	 * 
+	 * @parameter
+	 */
+	private String[] extensions;
+
+
+
+	/**
+	 * Run the goal.
+	 */
+	public void execute() throws MojoExecutionException {
+		// Check for pom.xml specified files
+		if ((this.paths == null) || (this.paths.length == 0)) {
+			this.getLog().info(GuiceyDataMojo.MESSAGE_NO_PATHS);
+			this.paths = new String[] { this.defaultFileDirectory.getAbsolutePath() };
+		}
+
+		// Check for pom.xml specified extensions
+		if ((this.extensions == null) || (this.extensions.length == 0)) {
+			this.getLog().info(GuiceyDataMojo.MESSAGE_NO_EXTENSIONS);
+			this.extensions = new String[] { GuiceyDataMojo.DEFAULT_EXTENSION };
+		}
+
+		this.getLog().info(GuiceyDataMojo.MESSAGE_GENERATING);
+
+		// Set up the generator with our values
+		final GuiceyDataGenerator generator = new GuiceyDataGenerator();
+		generator.setSourceDirectory(this.outputDirectory.getAbsolutePath());
+		generator.setOutputPackage(this.outputPackage);
+		generator.setFileExtensions(this.extensions);
+
+		// Hide output and error streams
+		System.setOut(null);
+		System.setErr(null);
+
+		try {
+			// Perform the generation
+			generator.generate(this.useCamelCase, this.paths);
+		} catch (final Exception e) {
+			this.getLog().error(GuiceyDataMojo.MESSAGE_ERROR);
+			throw new MojoExecutionException(GuiceyDataMojo.MESSAGE_ERROR, e);
+		}
+	}
 }
